1cbf8f4474da2d4ea2842b20fa568a26dea1a425
Patrice Chalin <pchalin@gmail.com>
2025-07-23 18:37:27 -0400

    Progress bar, snapshot

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -8,10 +8,51 @@ export type Size = 'small' | 'medium' | 'large';
 interface ProgressBarProps {
   value: number;
   size?: Size;
+  width?: number;
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const sizeProps: Record<Size, string> = {
+  small: `
+    height: 8px;
+  `,
+  medium: `
+    height: 12px;
+  `,
+  large: `
+    height: 24px;
+  `,
+};
+
+interface StyledProgressProps {
+  size?: Size;
+}
+
+const ProgressElt = styled.progress<StyledProgressProps>`
+  width: var(--width);
+  // box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  // border: none;
+  // border-color: ${COLORS.primary};
+  ${(props) => props.size && sizeProps[props.size]}
+`;
+
+const ProgressBar: React.FC<ProgressBarProps> = ({
+  value: _value,
+  size,
+  width = 160, // Match typical browser default
+}) => {
+  // Normalize the value to be between 0 and 100
+  const value = _value < 0 ? 0 : _value > 100 ? 100 : _value;
+
+  return (
+    <ProgressElt
+      value={value}
+      max={100}
+      style={{ '--width': width + 'px' } as React.CSSProperties}
+      size={size}
+    >
+      {value}
+    </ProgressElt>
+  );
 };
 
 export default ProgressBar;
