db293e7eb129a716a62a00a86f8950b46e059a2d
Patrice Chalin <pchalin@gmail.com>
2025-07-24 18:27:39 -0400

    Move size styling into SCSS

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -11,30 +11,6 @@ interface ProgressBarProps {
   width?: number;
 }
 
-type SizeProps = Record<
-  Size,
-  { height: string; borderRadius: string; padding: string }
->;
-
-const GRID_SIZE = '4px';
-const sizeProps: SizeProps = {
-  small: {
-    height: '8px',
-    borderRadius: GRID_SIZE,
-    padding: '0',
-  },
-  medium: {
-    height: '12px',
-    borderRadius: GRID_SIZE,
-    padding: '0',
-  },
-  large: {
-    height: '24px',
-    borderRadius: `calc(2 * ${GRID_SIZE})`,
-    padding: GRID_SIZE,
-  },
-};
-
 const minValue = 0;
 const maxValue = 100;
 const minWidth = 10;
@@ -51,19 +27,14 @@ const ProgressBar: React.FC<ProgressBarProps> = ({
     _value < minValue ? minValue : _value > maxValue ? maxValue : _value;
   const width = _width < minWidth ? minWidth : _width;
 
-  const sizeConfig = sizeProps[size];
-
   return (
     <div
       {...delegated}
-      className={styles.progressBar}
+      className={`${styles.progressBar} ${styles[size]}`}
       style={
         {
-          '--height': sizeConfig.height,
           '--width': width + 'px',
           '--bg-color': COLORS.transparentGray15,
-          '--border-radius': sizeConfig.borderRadius,
-          '--padding': sizeConfig.padding,
           '--primary-color': COLORS.primary,
           '--progress-width': value + '%',
         } as React.CSSProperties
