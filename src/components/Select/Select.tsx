f089bd17425bc6e0cded90a47d07471e97a51b49
Patrice Chalin <pchalin@gmail.com>
2025-07-24 21:12:46 -0400

    Update Select.tsx

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -13,41 +13,52 @@ interface SelectProps {
   children: React.ReactNode;
 }
 
+const SelectWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  min-height: 2.75rem;
+  cursor: pointer;
+
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border: 2px solid transparent; // Figma says gray700 but that's not what it looks like
+  border-radius: 8px;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
 const StyledSelect = styled.select`
   // Reset
   appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
 
-  // Custom
+  // min-height: 2.75rem;
+  padding: 0.75rem 1rem;
+  padding-right: 3.25rem;
+  background-color: transparent;
+  border: none;
+  border-radius: inherit;
+
+
+  color: inherit;
   font-size: 1rem;
   line-height: 1;
   font-weight: 400;
-  color: ${COLORS.gray700};
-  background-color: ${COLORS.transparentGray15};
-  border: 2px solid transparent; // Figma says gray700 but that's not what it looks like
-  border-radius: 8px;
 
-  padding: 0.75rem 1rem;
-  padding-right: 3.25rem;
-
-`;
-
-const SelectWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  height: 43px;
-  cursor: pointer;
-  &:hover {
-    color: ${COLORS.black};
-  }
 `;
 
 const SelectIcon = styled(Icon)`
   position: absolute;
-  right: 1rem;
-  margin-top: auto;
-  margin-bottom: auto;
+  top: 0;
+  bottom: 0;
+  right: 12px;
+  margin: auto 0;
+  height: 24px;
+  width: 24px;
+  pointer-events: none; // Allow clicks to pass through to select
 `;
 
 const Select: React.FC<SelectProps> = ({
@@ -64,7 +75,7 @@ const Select: React.FC<SelectProps> = ({
       <StyledSelect id={id} value={value} onChange={onChange}>
         {children}
       </StyledSelect>
-      <SelectIcon id="chevron-down" />
+      <SelectIcon id="chevron-down" strokeWidth={2} />
     </SelectWrapper>
   );
 };
